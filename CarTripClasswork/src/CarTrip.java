d3bc42a762e125a6a46bf301d14904de1b087923
kylea <kylea@DESKTOP-307U1LE.fios-router.home>
2020-02-03 22:49:38 -0500

    1.3

--- CarTripClasswork/src/CarTrip.java
+++ CarTripClasswork/src/CarTrip.java
@@ -1,6 +1,9 @@
+//By Kyle DeGuzman
+//Main Code
 public class CarTrip {
 
-private double myGallonsUsed, myStartOdometer, myEndOdometer, myTime;
+	//Class Members Instance Variables
+	private double myGallonsUsed, myStartOdometer, myEndOdometer, myTime;
 	  
 	  public CarTrip(){
 	    this.myStartOdometer = 1000.0;
@@ -14,35 +17,57 @@ private double myGallonsUsed, myStartOdometer, myEndOdometer, myTime;
 	    this.myTime = t;
 	    this.myGallonsUsed = g;
 	  }
+	  // Getter
+	  //i. myStartOdometer
+	  // Instance Variables
 	  public double getMyStartOdometer(){return this.myStartOdometer;
 	  }
+	  // Getter
+	  //ii. myEndOdometer
+	  // Instance Variables
 	  public double getMyEndOdometer(){return this.myEndOdometer;
 	  }
+	  // Getter
+	  //iv. myGallonsUsed
+	  // Instance Variables
 	  public double getMyGallonsUsed(){return this.myGallonsUsed;
 	  }
-	  public double getMyTime(){return this.myTime;}
+	  // Getter
+	  //iii. myTime
+	  // Instance Variables
+	  public double getMyTime(){return this.myTime;
+	  }
+	  //Class Members Instance Method 
 	  public void setMyStartOdometer(int so){this.myStartOdometer = so;
 	  }
+	  //Class Members Instance Method
 	  public void setMyEndOdometer(int eo){this.myEndOdometer = eo;
 	  }
+	  //Class Members Instance Method
 	  public void setMyGallonsUsed(double g){this.myGallonsUsed = g;
 	  }
+	  //Class Members Instance Method
 	  public void setMyTime(double t){this.myTime = t;
 	  }
+	  // ii. getTripDistance()
 	  public double getTripDistance(){
 	    return getMyEndOdometer() - getMyStartOdometer();
 	  }
+	  // iii. getAverageSpeed()
 	  public double getAverageSpeed(){
 	    double distance = getMyEndOdometer() - getMyStartOdometer();
 	    return distance /getMyTime();
 	  }
+	  // iv. getGasMileage()
 	  public double getGasMileage(){
 	    double distance = getMyEndOdometer() - getMyStartOdometer();
 	    return distance / getMyGallonsUsed();
 	  }
+	  // v. getTotalGasPrice(double pricePerGallon)
 	  public double getTotalGasPrice(double pricePerGallon){
 	    return getGasMileage() * pricePerGallon;
 	  }
+	  //Call the toString method with each object
 	  public String toString(){
 	    return "Start: " + getMyStartOdometer() + " miles End: " + getMyEndOdometer() + " miles Gallons used: " + getMyGallonsUsed() + " Time: " + getMyTime();
 	  }
--- CarTripClasswork/src/CarTripDriver.java
+++ CarTripClasswork/src/CarTripDriver.java
@@ -1,8 +1,10 @@
+//By Kyle DeGuzman
+//Driver Code
 public class CarTripDriver{
   public static void main(String[] args){
     CarTrip car1 = new CarTrip();
-    CarTrip car2 = new CarTrip(1542.5, 1800.0, 11.2, 6.1);
-    car1.setMyTime(20.2);
+    CarTrip car2 = new CarTrip(1542.5, 1800.0, 11.2, 7.1);
+    car1.setMyTime(30.2);
     car2.setMyTime(4.0);
     System.out.println("Trip Distance " + car1.getTripDistance() + " Average Speed: " + car1.getAverageSpeed() + " Gas Mileage: " + car1.getGasMileage() + " Total Gas Price: " + car1.getTotalGasPrice(1.50));
     System.out.println("Trip Distance " + car2.getTripDistance() + " Average Speed: " + car2.getAverageSpeed() + " Gas Mileage: " + car2.getGasMileage() + " Total Gas Price: " + car2.getTotalGasPrice(1.75));
